9dc895250ca730fe361156b5995e02d64eda67c0
alpha-99 <184989206@qq.com>
2026-02-28 18:44:21 +0800

    vector normalize

--- la/vector/Vector.py
+++ la/vector/Vector.py
@@ -1,6 +1,31 @@
+import math
+from ._globals import EPSILON
+
 class Vector:
     def __init__(self, lst):
         self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        """返回一个dim维度的零向量"""
+        return cls([0] * dim)
+
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalize(self):
+        """返回向量的单位向量"""
+        if self.norm() < EPSILON:
+            # less than EPSILON equals 0
+            raise ZeroDivisionError("Normalize error! norm is zero.")
+        return Vector(self._values) / self.norm()
+
+    def dot(self, another):
+        """向量点乘，返回结果标量"""
+        assert len(self) == len(another), \
+            "Error in dot product. Length of vectors must be same."
+        return sum(a * b for a, b in zip(self, another))
 
     def __add__(self, another):
         """向量加法，返回结果向量"""
@@ -23,6 +48,10 @@ class Vector:
     def __rmul__(self, k):
          """返回数量乘法的结果向量：k * self"""
          return self * k
+
+    def __truediv__(self, k):
+        """返回数量除法的结果向量 self / k"""
+        return (1 / k) * self
 
     def __pos__(self):
         """返回向量取正的结果向量"""
